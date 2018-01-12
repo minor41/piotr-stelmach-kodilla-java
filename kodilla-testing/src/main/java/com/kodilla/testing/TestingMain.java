364dd8260d19292462b480f67db8c8c64434c640
piotrstelmach <minor41@gmail.com>
2018-01-12 23:40:25 +0100

    First TDD

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,43 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calulator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        // tu umieść kolejny test ->
+        System.out.println("Test - pierwszy test jednostkowy:" + "\n");
+
+        Calculator calculator = new Calculator();
+
+        int result2 = calculator.add(5,7);
+
+        int result3 = calculator.subtract(9,3);
+
+        if (calculator.equals(calculator)) {
+            System.out.println("result of adding equals: " + result2 + "\n");
+        } else {
+            System.out.println("calculator is not working");
+        }
+
+        if (calculator.equals(calculator)){
+            System.out.println("result of subtracting equals: " + result3);
+        } else {
+            System.out.println("calculator is not working");
+        }
+
+
+
+
     }
 }
