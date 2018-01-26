ca1e93e8f721949fcf0981c80d54dcc9fa160254
piotrstelmach <minor41@gmail.com>
2018-01-26 17:09:01 +0100

    book library

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,12 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.lambda.SaySamothing;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to module 7 - Stream");
+
+        SaySamothing saySamothing = new SaySamothing();
+        saySamothing.say();
+    }
+}
--- kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static com.sun.tools.internal.xjc.reader.Ring.add;
+
 
 public class BookLibrary {
     private LibraryDatabase libraryDatabase;
