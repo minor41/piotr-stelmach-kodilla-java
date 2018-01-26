4e2f354d666751009f8175236e58828a4efb565a
piotrstelmach <minor41@gmail.com>
2018-01-26 17:31:08 +0100

    lambda

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,12 +1,15 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.lambda.SaySamothing;
+import com.kodilla.stream.lambda.ExecuteSaySomething;
+import com.kodilla.stream.lambda.Processor;
 
 public class StreamMain {
     public static void main(String[] args) {
         System.out.println("Welcome to module 7 - Stream");
 
-        SaySamothing saySamothing = new SaySamothing();
-        saySamothing.say();
+        Processor processor = new Processor();
+        ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
+        processor.execute(executeSaySomething);
     }
 }
