c4496bf4d430388725c8a5f74d5e52fb6e4a851b
piotrstelmach <minor41@gmail.com>
2018-01-27 22:58:43 +0100

    forum user

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,9 +1,12 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.book.BookDirectory;
 import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
+import com.kodilla.stream.person.People;
+
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -27,14 +30,28 @@ public class StreamMain {
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
 
+
+        // zadanie 7,1
         PoemBeautifier poemBeautifier = new PoemBeautifier();
         poemBeautifier.beautify("Hello", (abc) -> abc);
         poemBeautifier.beautify("Hello", (abc) -> abc.replaceFirst("Hello", "Bye"));
-        poemBeautifier.beautify("HI ", (abc) -> abc.toLowerCase());
+        poemBeautifier.beautify("HI ", (abc -> abc.toLowerCase()));
         poemBeautifier.beautify(" Hmmm ", (abc) -> abc.replace('m', 'H'));
 
         System.out.println("Using Stream to generate even numbers from 1 to 20");
         NumbersGenerator.generateEven(20);
+
+        People.getList().stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > 11)
+                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
+                .filter(s -> s.substring(0, 1).equals("M"))
+                .forEach(System.out::println);
+
+        BookDirectory theBookDirectory = new BookDirectory();
+        theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .forEach(System.out::println);
 
     }
 }
