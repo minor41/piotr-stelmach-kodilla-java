cad8d1d9089c9e0f9ae695e9c79ed4787b0b8e28
piotrstelmach <minor41@gmail.com>
2018-01-29 22:50:19 +0100

    Big Decimal

--- kodilla-stream/src/main/java/com/kodilla/stream/world/Country.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/Country.java
@@ -3,10 +3,27 @@ package com.kodilla.stream.world;
 import java.math.BigDecimal;
 
 public class Country {
+    private final String nameOftheCountry;
+    private final BigDecimal numberOfPeopleInTheCountry;
 
-    BigDecimal numberOfPeopleInTheCountry;
+    public Country(final String nameOftheCountry,final BigDecimal numberOfPeopleInTheCountry){
+        this.nameOftheCountry = nameOftheCountry;
+        this.numberOfPeopleInTheCountry = numberOfPeopleInTheCountry;
+    }
 
-     BigDecimal getPeopleQuantity() {
+    public String getNameOftheCountry(){
+        return nameOftheCountry;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "nameOftheCountry='" + nameOftheCountry + '\'' +
+                ", numberOfPeopleInTheCountry=" + numberOfPeopleInTheCountry +
+                '}';
+    }
+
+    public BigDecimal getPeopleQuantity() {
         return numberOfPeopleInTheCountry;
     }
 }
