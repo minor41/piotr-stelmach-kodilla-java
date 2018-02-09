1ea65fa9513ed7d03cc2aead8b35bb4ded6ea063
piotrstelmach <minor41@gmail.com>
2018-02-09 19:54:02 +0100

    8.4

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FindFlight.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FindFlight.java
@@ -4,8 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FindFlight {
+    private Flight flight;
 
     public void findFlight(Flight flight) throws RouteNotFoundException{
+        this.flight = flight;
+
         if (flight != null) {
             final Map<String, Boolean> findFlight = new HashMap<>();
             findFlight.put("new york", true);
@@ -27,18 +30,18 @@ class Test{
 
     public static void main(String args[]) {
 
-   //    FindFlight lookForFlight = new FindFlight();
-   //
-   //    try {
-   //        lookForFlight.findFlight();
-   //
-   //    } catch (RouteNotFoundException e) {
-   //
-   //        System.out.println("Route not found");
-   //
-   //    }finally {
-   //
-   //        System.out.println("Thank you for using our service");
-   //    }
+       FindFlight lookForFlight = new FindFlight();
+
+       try {
+           lookForFlight.findFlight();
+
+       } catch (RouteNotFoundException e) {
+
+           System.out.println("Route not found");
+
+       }finally {
+
+           System.out.println("Thank you for using our service");
+       }
     }
 }
