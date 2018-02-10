e34eb1463d546155156bddd5d410f61e0561c927
piotrstelmach <minor41@gmail.com>
2018-02-10 16:00:13 +0100

    8.4

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightMap.java
@@ -0,0 +1,67 @@
+package com.kodilla.exception.test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FlightMap {
+
+    private final Map<String, List<String>> flightMap = new HashMap<>();
+
+    public void addNewFlight(Flight flight) {
+        if(flightMap.get(flight.getDepartureAirport()) == null) {
+            flightMap.put(flight.getDepartureAirport(), new ArrayList<>());
+        }
+
+        final List<String> flightsFromDepartureAirport = flightMap.get(flight.getDepartureAirport());
+
+        flightsFromDepartureAirport.add(flight.getArrivalAirport());
+    }
+
+
+    public Boolean findFlight(String departure, String arrival) throws RouteNotFoundException{
+
+        List<String> flightsFromDepartureAirport = (List<String>) this.flightMap;
+
+        if (flightsFromDepartureAirport != null || flightsFromDepartureAirport.contains(arrival)){
+            return true;
+        }
+        if (flightsFromDepartureAirport.contains(arrival)){
+            return false;
+        }
+  
+        for(String flight : flightsFromDepartureAirport){
+            if (findFlight(flight, arrival)) {
+                return true;
+            }
+        }
+  
+        throw new RouteNotFoundException();
+  
+    }
+}
+
+class Test {
+
+    public static void main(String args[]) {
+
+        final FlightMap flightMap = new FlightMap();
+
+        flightMap.addNewFlight(new Flight("Warsaw", "Berlin"));
+        flightMap.addNewFlight(new Flight("Warsaw", "London"));
+        flightMap.addNewFlight(new Flight("London", "New York"));
+        flightMap.addNewFlight(new Flight("New York", "Chicago"));
+
+        try {
+
+            final boolean flight = flightMap.findFlight("Warsaw", "Chicago");
+            System.out.println("Flight: " + flight);
+
+        } catch (RouteNotFoundException e) {
+            System.out.println("Can't find flight from Warsaw to Chicago");
+        }finally {
+            System.out.println("Thank you for using our service");
+        }
+    }
+}
