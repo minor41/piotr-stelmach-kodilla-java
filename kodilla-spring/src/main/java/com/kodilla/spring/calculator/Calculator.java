4e6025f1959ca9abf9acc78adc4ead44225465b1
piotrstelmach <minor41@gmail.com>
2018-02-23 12:12:08 +0100

    10.2

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -1,16 +1,14 @@
 package com.kodilla.spring.calculator;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Calculator {
 
+    @Autowired
     private Display display;
-
-    public Calculator(Display display){
-        this.display = display;
-    }
 
     public double add(double a, double b) {
         display.displayValue(a + b);
