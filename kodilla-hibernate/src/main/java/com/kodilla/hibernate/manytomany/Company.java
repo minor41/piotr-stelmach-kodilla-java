3432474cd10802a32fa1caef5d5757eef5e0762c
piotrstelmach <minor41@gmail.com>
2018-03-19 22:13:16 +0100

    13.4

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @NamedNativeQuery(
-        name = "Company.retriveCompanyName",
+        name = "Company.retrieveCompanyName",
         query = "SELECT * FROM COMPANIES" +
                 " WHERE MID(COMPANY_NAME, 1, 3) = :THREECHARACTERS",
         resultClass = Company.class
